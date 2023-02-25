8f897cfb6683b07ede9cf758ab576bf94ef41506
0HugoHu <0@hugohu.top>
2023-02-24 21:36:50 -0500

    First try with Fragment Template

---
+++ app/src/main/java/edu/duke/risc/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.duke.risc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.duke.risc.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
---
+++ app/src/main/java/edu/duke/risc/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package edu.duke.risc.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
