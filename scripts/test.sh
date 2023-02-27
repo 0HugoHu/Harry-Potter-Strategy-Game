5a823846cec39b9c2be9b1f4669672cd5a7e5b20
0HugoHu <0@hugohu.top>
2023-02-27 18:25:27 -0500

    Resolve permission issue again and again...

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./gradlew build || exit 1
-./gradlew cloverGenerateReport || exit 1
+./gradlew :server:build || exit 1
+./gradlew :server:cloverGenerateReport || exit 1
 scripts/coverage_summary.sh
 cp -r app/build/reports/clover/html/* /coverage-out/ || exit 1
 
