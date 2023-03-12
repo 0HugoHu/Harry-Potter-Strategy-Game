ecc503f75dfaa2cd6d9eb5799fd5c3fc0f36aa00
0HugoHu <0@hugohu.top>
2023-03-11 20:11:17 -0500

    Gitlab CI test

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,8 +5,9 @@ RUN chmod +x gradlew
 ./gradlew :client:build || exit 1
 ./gradlew :client:cloverGenerateReport || exit 1
 scripts/coverage_summary.sh
-mkdir -p -m 777 /coverage-out/client
-mkdir -p -m 777 /coverage-out/server
+mkdir -m 777 /coverage-out
+mkdir -m 777 /coverage-out/client
+mkdir -m 777 /coverage-out/server
 chmod +x /coverage-out/client
 chmod +x /coverage-out/server
 chmod +x server/build/reports/clover/html/*
