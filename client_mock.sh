577632fb908774300d5cc55e39796af3b6f37108
hugohu <0>
2023-04-11 15:44:00 -0400

    Changed num of players, re-configured remote server IP.

--- client_mock.sh
+++ client_mock.sh
@@ -1,3 +1,4 @@
 ./gradlew :client:build -x test
-./gradlew :client:run --args="192.168.1.134 mock" &
-./gradlew :client:run --args="192.168.1.134 mock" &
+./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
+./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
+./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
