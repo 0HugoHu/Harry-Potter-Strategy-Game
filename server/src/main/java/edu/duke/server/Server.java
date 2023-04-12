d94717ccb5bc704d1d15703d2670ec0633ab428b
0HugoHu <0@hugohu.top>
2023-04-12 00:41:16 -0400

    Solved some issues.

--- server/src/main/java/edu/duke/server/Server.java
+++ server/src/main/java/edu/duke/server/Server.java
@@ -25,7 +25,7 @@ public class Server {
     // Port number
     private final static int PORT = 5410;
     // Number of players
-    private final static int numPlayers = 3;
+    private final static int numPlayers = 4;
     // Number of units at the beginning
     private final static int numUnits = 24;
     // Gameplay controller
