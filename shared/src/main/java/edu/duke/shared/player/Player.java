8d372ad6106ba52f085bd931f382f0aba5a7d20b
hugohu <0>
2023-04-11 20:53:12 -0400

    Solved some issues.

--- shared/src/main/java/edu/duke/shared/player/Player.java
+++ shared/src/main/java/edu/duke/shared/player/Player.java
@@ -262,7 +262,6 @@ public class Player implements Serializable {
 
     public void upgradeWorldLevel() {
         this.worldLevel++;
-        this.horns -= upgradeCost(this.worldLevel);
     }
 
     /**
