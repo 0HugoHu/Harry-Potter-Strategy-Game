4def60928010bf9a1cee79ab416c412f12f3df24
0HugoHu <0@hugohu.top>
2023-04-19 11:54:59 -0400

    Now randomly initialize house.

--- client_mock.sh
+++ client_mock.sh
@@ -1,4 +1,7 @@
 ./gradlew :client:build -x test
-./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
-./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
-./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
+#./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
+#./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
+#./gradlew :client:run --args="vcm-30577.vm.duke.edu mock" &
+./gradlew :client:run --args="10.198.110.177 mock" &
+./gradlew :client:run --args="10.198.110.177 mock" &
+./gradlew :client:run --args="10.198.110.177 mock" &
