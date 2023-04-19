23922b7355b21b6806737e555a27e20816f186c4
0HugoHu <0@hugohu.top>
2023-04-18 21:47:15 -0400

    Evolution 3 base

---
+++ shared/src/main/java/edu/duke/shared/player/House.java
@@ -0,0 +1,8 @@
+package edu.duke.shared.player;
+
+public enum House {
+    RAVENCLAW,
+    HUFFLEPUFF,
+    GRYFFINDOR,
+    SLYTHERIN
+}
---
+++ shared/src/main/java/edu/duke/shared/player/SkillState.java
@@ -0,0 +1,10 @@
+package edu.duke.shared.player;
+
+public enum SkillState {
+    // Skill is not used
+    NOT_USED,
+    // Skill is in effect
+    IN_EFFECT,
+    // Skill is used
+    USED
+}
